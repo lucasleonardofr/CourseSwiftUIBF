1b32f3257ba07bf130172d1bcd2c7d07ac4157cb
lucasleonardofr <lucasleonardofr@gmail.com>
2025-05-23 08:07:55 -0300

    Initial Commit

---
+++ AppSwiftUIBF/AppSwiftUIBFApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSwiftUIBFApp.swift
+//  AppSwiftUIBF
+//
+//  Created by Lucas Leonardo Freitas on 23/05/25.
+//
+
+import SwiftUI
+
+@main
+struct AppSwiftUIBFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppSwiftUIBF/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppSwiftUIBF
+//
+//  Created by Lucas Leonardo Freitas on 23/05/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
